443fdba334860b963c633ce662edb3c6e93cbdf5
Benjamin Medina <catiremedina@gmail.com>
2022-02-09 12:30:18 -0500

    esta me jodio

--- src/app.js
+++ src/app.js
@@ -1,11 +1,46 @@
-/* eslint-disable */
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const RandomCard = () => {
+    let number = Math.floor(Math.random() * 12) + 1;
+    let suit = Math.floor(Math.random() * 4) + 1;
+    let casesuit = "";
+    let numero = document.getElementsByClassName("number")[0];
+
+    switch (suit) {
+      case 1:
+        casesuit = "♥";
+        break;
+      case 2:
+        casesuit = "♠";
+        break;
+      case 3:
+        casesuit = "♦";
+        break;
+      case 4:
+        casesuit = "♣";
+        break;
+    }
+    switch (numero) {
+      case 1:
+        numero = "A";
+        break;
+      case 10:
+        numero = "J";
+        break;
+      case 11:
+        numero = "Q";
+        break;
+      case 12:
+        numero = "K";
+        break;
+    }
+    numero.innerHTML = number;
+
+    let card = document.getElementsByClassName("card")[0];
+
+    card.classList = `card ${casesuit}`;
+  };
+  RandomCard();
 };
